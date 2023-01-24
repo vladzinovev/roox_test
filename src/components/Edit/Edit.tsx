1f3e059743a105b49e524ca0d2a9012901dced24
Vladislav  Zinovyev <vladozdeveloper@gmail.com>
2023-01-24 23:47:06 +0300

    fixed

--- src/components/Edit/Edit.tsx
+++ src/components/Edit/Edit.tsx
@@ -9,30 +9,80 @@ const Edit = () => {
   let params = useParams();
   const [edit, setEdit] = useState(false);
   const [postItem, setPostItem] = useState<IPost>();
-
+  const [array, setArray] = useState<IPost[]>([{
+    id: 8,
+    name: "Nicholas Runolfsdottir V",
+    username: "Maxime_Nienow",
+    email: "Sherwood@rosamond.me",
+    address: {
+      street: "Ellsworth Summit",
+      suite: "Suite 729",
+      city: "Aliyaview",
+      zipcode: "45169",
+      geo: {
+        lat: "-14.3990",
+        lng: "-120.7677"
+      }
+    },
+    phone: "586.493.6943 x140",
+    website: "jacynthe.com",
+    company: {
+      name: "Abernathy Group",
+      catchPhrase: "Implemented secondary concept",
+      bs: "e-enable extensible e-tailers"
+    }
+  }]);
   function getUserId() {
     setPostItem(post.find((post) => post.id === Number(params.id)));
   }
+  /* function get(){
+    let copy = Object.assign([], array);
+    console.log(postItem);
+    if(postItem!==undefined){
+        copy.push(postItem);
+        setArray(copy);
+    }
+    console.log(array)
+  } */
 
   const onEdit = () => {
-    setEdit(true);
+    setEdit(!edit);
   };
 
-  const handleSubmit = (e: any) => {
+  function handleSubmit (e:any){
     e.preventDefault();
+    onEdit();
+     /* setPostItem(oldValues=>({...oldValues,[e.target.elements.name]:e.target.elements.name.value})); */
+    const target=e.target.elements;
+    const names:string = target.name.value;
+    const userName = target.user_name.value;
+    const email = target.email.value;
+    const street = target.street.value;
+    const city = target.city.value;
+    const zipcode = target.zip_code.value;
+    const phone = target.phone.value;
+    const website = target.website.value;
+    const comment = target.comment.value;
+    console.log(`${names} ${userName} ${email} ${street} ${city} ${zipcode} ${phone} ${website} ${comment} `)
 
-    const name = e.target.elements.name.value;
+    setArray(array.map(obj=>{
+        
+        if(obj.id===Number(params.id)){
+        console.log(params.id);
+        console.log(obj.id);
+        console.log(obj.name);
+        console.log([obj.name]);
+            return {...obj,['name']:names};
+        } else { 
+            return obj;
+        }
+    }));
+    console.log(array);
   };
+
   useEffect(() => {
-    console.log(params);
     getUserId();
-    /* fetchUser(); */
-    console.log(postItem);
   }, [params.id]);
-  /* useEffect(() => {
-    
-    console.log(postItem);
-  }, [postItem]); */
 
   return (
     <div className={styles.edit}>
@@ -41,13 +91,15 @@ const Edit = () => {
         <button
           onClick={onEdit}
           disabled={edit ? true : false}
-          className={`${styles.btn_edit} ${!edit?styles.btn_enabled:styles.btn_disabled}`}
+          className={`${styles.btn_edit} ${
+            !edit ? styles.btn_enabled : styles.btn_disabled
+          }`}
         >
           Редактировать
         </button>
       </div>
 
-      <form className={styles.form} onSubmit={handleSubmit}>
+      <form className={styles.form} onSubmit={(e)=>handleSubmit(e)}>
         <div>
           <label className={styles.label_input} htmlFor="name">
             User
@@ -57,9 +109,9 @@ const Edit = () => {
             type="text"
             name="name"
             id="name"
-            disabled={!edit ? true : false}
             defaultValue={postItem?.name}
             required
+            readOnly={!edit ? true : false}
           />
         </div>
         <div>
@@ -71,7 +123,7 @@ const Edit = () => {
             type="text"
             name="user_name"
             id="user_name"
-            disabled={!edit ? true : false}
+            readOnly={!edit ? true : false}
             defaultValue={postItem?.username}
             required
           />
@@ -85,7 +137,7 @@ const Edit = () => {
             type="email"
             name="email"
             id="email"
-            disabled={!edit ? true : false}
+            readOnly={!edit ? true : false}
             defaultValue={postItem?.email}
             required
           />
@@ -99,7 +151,7 @@ const Edit = () => {
             type="text"
             name="street"
             id="street"
-            disabled={!edit ? true : false}
+            readOnly={!edit ? true : false}
             defaultValue={postItem?.address.street}
             required
           />
@@ -113,7 +165,7 @@ const Edit = () => {
             type="text"
             name="city"
             id="city"
-            disabled={!edit ? true : false}
+            readOnly={!edit ? true : false}
             defaultValue={postItem?.address.city}
             required
           />
@@ -127,7 +179,7 @@ const Edit = () => {
             type="text"
             name="zip_code"
             id="zip_code"
-            disabled={!edit ? true : false}
+            readOnly={!edit ? true : false}
             defaultValue={postItem?.address.zipcode}
             required
           />
@@ -141,7 +193,7 @@ const Edit = () => {
             type="phone"
             name="phone"
             id="phone"
-            disabled={!edit ? true : false}
+            readOnly={!edit ? true : false}
             defaultValue={postItem?.phone}
             required
           />
@@ -155,7 +207,7 @@ const Edit = () => {
             type="text"
             name="website"
             id="website"
-            disabled={!edit ? true : false}
+            readOnly={!edit ? true : false}
             defaultValue={postItem?.website}
             required
           />
@@ -169,7 +221,7 @@ const Edit = () => {
             type="text"
             name="comment"
             id="comment"
-            disabled={!edit ? true : false}
+            readOnly={!edit ? true : false}
           />
         </div>
         <button
