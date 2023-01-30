997fc367f9003bbebd184d4ea8749f52779fac90
Vladislav  Zinovyev <vladozdeveloper@gmail.com>
2023-01-31 00:29:15 +0300

    roox_test 2.0

--- src/hook/useTypedSelector.ts
+++
@@ -1,6 +0,0 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
-import { AppDispatch, TypeRootState } from "../store";
-
-export const useTypedSelector: TypedUseSelectorHook<TypeRootState> =
-  useSelector;
-export const useAppDispatch: () => AppDispatch = useDispatch;
